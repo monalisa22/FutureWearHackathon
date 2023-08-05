83dae6343543a26d4fc102c857f4e338411cd135
misramrinal <108413144+misramrinal@users.noreply.github.com>
2023-08-05 07:37:11 +0530

    Final Touchups

--- login.py
+++ login.py
@@ -30,35 +30,17 @@ firebas=pyrebase.initialize_app(config)
 authe = firebas.auth()
 database = firebas.database()
 
-# def model(request):
-    
-#     global data_sys,data_dia,data_bp,data_temp,data_HR,data_SD,data_step
-#     data_sys= database.child('esp32_data').child('systolic').get().val()
-#     data_dia= database.child('esp32_data').child('diastolic').get().val()
-#     data_bp= database.child('esp32_data').child('bp').get().val()
-#     data_temp= database.child('esp32_data').child('temperature').get().val()
-#     data_HR= database.child('esp32_data').child('Heartrate').get().val()
-#     data_SD= database.child('esp32_data').child('sleep disorder').get().val()
-#     data_step= database.child('esp32_data').child('steps').get().val()
-#     return render(request,'index.html', {
-#         "systolic":data_sys,
-#         "diastolic":data_dia,
-#         "bp":data_bp,
-#         "temperature":data_temp,
-#         "sleep disorder":data_SD,
-#         "heartrate":data_HR,
-#         "steps":data_step,
-#     }
-#     )
 age=0
 weighht=0
 height=0
 gender=""
 sleep_duration=0
 
+# This is the landing page
 @app.route('/')
 def home():
     return render_template('dashboard.html')
+
 @app.route('/home')
 def homei():
     return render_template('dashboard.html')
@@ -87,17 +69,19 @@ def login():
     flash("That User Doesn't Exist! Try Again...")
     return render_template('login.html')
 
-
+# This will be triggered when the user signin is successfull
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
     return render_template('dashboard2.html')
 
+# This fuction is used to predict the stress level and fitness score based on the input parameters
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     global age, gender, sleep_duration, height, weight
+    
+    # Getting input parameters from the database
     data_sys= database.child('systolic').get().val()
     data_dia= database.child('diastolic').get().val()
-    # data_bp= database.child('bp').get().val()
     data_temp= database.child('Temperature').get().val()
     data_HR= database.child('heartrate').get().val()
     data_SD= database.child('sleep_disorder').get().val()
@@ -105,7 +89,6 @@ def predict():
     
     data_sys = OrderedDict(data_sys)
     data_dia = OrderedDict(data_dia)
-    # data_bp = OrderedDict(data_bp)
     data_temp = OrderedDict(data_temp)
     data_HR = OrderedDict(data_HR)
     data_SD = OrderedDict(data_SD)
@@ -113,30 +96,35 @@ def predict():
     
     value_sys = float(data_sys['data'])
     value_dia = float(data_dia['data'])
-    # value_bp = float(data_bp['data'])
     value_temp = float(data_temp['data']) + 21
     value_HR = float(data_HR['data'])
     value_SD = str(data_SD['data'])
     value_step = float(data_step['data'])
     
+    # Converting 'Male' and 'Female' keyword into numerical value
     with open('gender_mapping.pkl', 'rb') as f:
         vocab = pickle.load(f)
     gendr = vocab[gender]
     
+    # Converting sleep disorder values into numerical values
     with open('sleep_mapping.pkl', 'rb') as f:
         vocab = pickle.load(f)
     SD=vocab[value_SD]
     
+    
+    # Getting BMI of the user
     height_meters=height/100
     Bmi=weight/(height_meters*height_meters)
     Bmi=round(Bmi,2)
     
+    # Sending the input parameters into the trained model
     data = [[gendr, age, sleep_duration, value_sys, value_dia, value_HR, value_temp, value_step, SD]]
     input_df = pd.DataFrame(data, columns=['Gender', 'Age', 'Sleep Duration', 'Systolic', 'Diastolic', 'Heart Rate', 'Temperature', 'Daily Steps', 'Sleep Disorder'])
     prediction = model.predict(input_df)
     score = model.predict_proba(input_df)
     prediction=int(prediction)
     
+    # Predicting the fitness score
     fitness_negative = (abs(value_HR - 70))*0.67 + (abs(Bmi - 22))*1 + (abs(value_sys - 120))*0.1 + (abs(value_dia - 80))*0.09 + (prediction)*1 + SD*(0.18) + (abs(value_step - 10000))*0.02 + (abs(value_temp - 98.6))*0.08
     fitness_positive = (sleep_duration-8)*0.81
     fitness_score = 100 - fitness_negative + fitness_positive
@@ -147,6 +135,8 @@ def predict():
     temp_text=""
     Bmi_text=""
     
+    
+    # Some more optimizations
     if(value_HR < 60 or value_HR > 100):
         HR_text = f"We detected abnormal heart rate from your readings. Your heart rate reading is {value_HR}"
         flash(HR_text)
@@ -165,10 +155,9 @@ def predict():
         
     if(value_temp < 95 or value_temp > 101):
         temp_text = f"We detected abnormal temperature from your readings. Your temperature reading is {value_temp}"
-        flash(temp_text)
+        flash(temp_text )
         
     return render_template('index.html', prediction_text=prediction, input_text=Bmi, fitness_text=fitness_score, HR_text=HR_text, sys_text=sys_text, dia_text=dia_text, Bmi_text=Bmi_text, temp_text=temp_text)
-    # return render_template("index.html", data_sys=age)
 
 # Create Logout Page
 @app.route('/logout', methods=['GET', 'POST'])
@@ -177,6 +166,7 @@ def logout():
 	return redirect(url_for('home'))
 
 
+# Create Sign UP page
 @app.route('/signin', methods=["GET", "POST"])
 def signin():
     if request.method == 'POST' and len(dict(request.form)) > 0:
@@ -191,8 +181,6 @@ def signin():
         sleep_duration = userdata["sleep_duration"]
         
         result = firebase.get('/user', None)
-        # username = request.form["Username"]
-         # Convert dict_values to a list of dictionaries
         user_list = list(result.values())
 
         # Check if the provided username exists in any dictionary
